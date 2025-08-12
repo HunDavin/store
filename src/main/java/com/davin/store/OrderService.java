04ea5d5979bb1ddfd99b2420c386f880b2501890
HunDavin <davin.hun@student.cadt.edu.kh>
2025-08-13 05:38:45 +0700

    setter injection: a setter function for inject dependency (no reccomanded)

--- src/main/java/com/davin/store/OrderService.java
+++ src/main/java/com/davin/store/OrderService.java
@@ -4,10 +4,14 @@ public class OrderService {
     private PaymentService paymentService;
 
 
-    public OrderService(PaymentService paymentService){
-        this.paymentService = paymentService;
-    }
+    // public OrderService(PaymentService paymentService){
+    //     this.paymentService = paymentService;
+    // }
     public void placeOrder(){
         paymentService.processPayment(20);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
--- src/main/java/com/davin/store/StoreApplication.java
+++ src/main/java/com/davin/store/StoreApplication.java
@@ -7,7 +7,8 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(StoreApplication.class, args);
-		var OrderService = new OrderService(new stripePaymentService());
+		var OrderService = new OrderService();
+		OrderService.setPaymentService(new PaypalPaymentService());
 		OrderService.placeOrder();
 
 	}
