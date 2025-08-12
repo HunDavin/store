3eb1452c25eeae7647f701d827adc4eff7597be6
HunDavin <davin.hun@student.cadt.edu.kh>
2025-08-13 05:50:53 +0700

    using spring

--- src/main/java/com/davin/store/StoreApplication.java
+++ src/main/java/com/davin/store/StoreApplication.java
@@ -1,15 +1,16 @@
 package com.davin.store;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(StoreApplication.class, args);
-		var OrderService = new OrderService(new PaypalPaymentService());
-		// OrderService.setPaymentService(new PaypalPaymentService());
-		OrderService.placeOrder();
+		SpringApplication.run(StoreApplication.class, args);
+		// var OrderService = new OrderService(new PaypalPaymentService());
+		// // OrderService.setPaymentService(new PaypalPaymentService());
+		// OrderService.placeOrder();
 
 	}
 
