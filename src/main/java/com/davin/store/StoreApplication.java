f770db8978378c2dd13b7c124efe0e500b54c690
HunDavin <davin.hun@student.cadt.edu.kh>
2025-08-13 05:41:05 +0700

    back to Construtor injection

--- src/main/java/com/davin/store/StoreApplication.java
+++ src/main/java/com/davin/store/StoreApplication.java
@@ -7,8 +7,8 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(StoreApplication.class, args);
-		var OrderService = new OrderService();
-		OrderService.setPaymentService(new PaypalPaymentService());
+		var OrderService = new OrderService(new PaypalPaymentService());
+		// OrderService.setPaymentService(new PaypalPaymentService());
 		OrderService.placeOrder();
 
 	}
