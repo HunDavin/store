edc622980f4764642339f260881e3f52bd857fd4
HunDavin <davin.hun@student.cadt.edu.kh>
2025-08-13 03:25:57 +0700

    first commit

---
+++ src/test/java/com/davin/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davin.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
